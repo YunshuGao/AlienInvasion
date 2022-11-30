84872f113444e68a01dec9f9c90ce66eecc90838
YunshuGao <gaobaiding@gmail.com>
2022-11-30 22:50:58 +0800

    Creating the Ship Class 不知道bug在哪里

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Over class to manage game assets and behavior"""
@@ -7,11 +9,11 @@ class AlienInvasion:
     def __init__(self):
         """initial the game, and create game resources."""
         pygame.init()
-
-        self.screen=pygame.display.set_mode((1200,800))
+        self.settings=Settings()
+        self.ship=Ship(self)
+        self.screen=pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        #Set the backgroud color.
-        self.bg_color=(230,230,230)
+
 
 
     def run_game(self):
@@ -21,7 +23,9 @@ class AlienInvasion:
                 if event.type==pygame.QUIT:
                     sys.exit()
             #Redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+
+            self.ship.blitme()
 
             #Make the most recently drawn screen visible.
             pygame.display.flip()
