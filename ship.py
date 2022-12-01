638718cc7a3756986e9be2a6c863376775811a54
YunshuGao <gaobaiding@gmail.com>
2022-12-01 16:04:03 +0800

    Responding to a Keypress

--- ship.py
+++ ship.py
@@ -8,9 +8,9 @@ class Ship:
         self.screen=ai_game.screen
         self.screen_rect=ai_game.screen.get_rect()
 
-        self.image=pygame.image.load('images/ship.bmp')
-        self.image_rect=self.image.get_rect()
-        self.image_rect.midbottom=self.screen_rect.midbottom
+        self.ship_image=pygame.image.load('images/ship.bmp')
+        self.ship_rect=self.ship_image.get_rect()
+        self.ship_rect.midbottom=self.screen_rect.midbottom
 
     def blitme(self):
-        self.screen.blit(self.image,self.image_rect)
+        self.screen.blit(self.ship_image,self.ship_rect)
